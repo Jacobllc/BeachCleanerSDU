28d036097efbc42d88b9d5661e256dde8dcdee54
Jacobllc <57322518+Jacobllc@users.noreply.github.com>
2019-11-11 20:09:18 +0100

    Update of slave

--- 3. Communication/i2c/i2c_atmega_328p_slave/i2c_atmega_328p_slave/i2c_atmega_328p_slave.c
+++ 3. Communication/i2c/i2c_atmega_328p_slave/i2c_atmega_328p_slave/i2c_atmega_328p_slave.c
@@ -10,48 +10,45 @@
 #include <stdio.h>
 #include "i2c_atmega_328p_slave.h"
 
-
-static void (*I2C_recieve)(uint8_t);		
-static void (*I2C_data_request)();
-
-void I2C_setCallbacks(void (*reciece_call)(uint8_t), void (*request_call)())
+void I2C_recieve(uint8_t received_data)							//isr on receiving a byte on i2c
+ {
+	 data = received_data;
+	 data2=data+2;													//received data
+	 printf("From master: %d\n",data);											
+ }		
+ 
+void I2C_data_request()												//if master request data from slave
 {
-	I2C_recieve = reciece_call;
-	I2C_data_request = request_call;
+	i2c_transmit_data(data2);
 }
 
 void i2c_init(uint8_t address)
 {
-	
 		cli();						
 		TWAR = address << 1;										// load address into TWI address register
 		TWCR = (1<<TWIE) | (1<<TWEA) | (1<<TWINT) | (1<<TWEN);		// set the TWCR to enable address matching and enable TWI, clear TWINT, enable TWI interrupt
 		sei();														//enable TWI interrupt
 }
 
-
 void i2c_disable(void)
 {
-		
 		cli();
 		TWCR = 0;			// clear acknowledge and enable bits
 		TWAR = 0;
 		sei();
 }
 
-
 void i2c_transmit_data(int data)
-{	
-	
+{		
 	TWDR = data;
 }
  
-
 ISR(TWI_vect)
 {
 	switch(TW_STATUS)
 	{
 		case TW_SR_DATA_ACK:
+		printf("TW_SR_DATA_ACK\n");
 		// received data from master, call the receive callback
 		I2C_recieve(TWDR);
 		
@@ -60,15 +57,16 @@ ISR(TWI_vect)
 		
 		case TW_ST_SLA_ACK:
 		// master is requesting data, call the request callback
+		printf("TW_ST_SLA_ACK\n");
 		I2C_data_request();
 		TWCR = (1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
 		break;
 		
 		case TW_ST_DATA_ACK:
 		// master is requesting data, call the request callback
-	
+		printf("TW_ST_DATA_ACK\n");
+		I2C_data_request();
 		TWCR = (1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (1<<TWEN);
-		
 		break;
 		
 		case TW_BUS_ERROR:
--- 3. Communication/i2c/i2c_atmega_328p_slave/i2c_atmega_328p_slave/i2c_atmega_328p_slave.h
+++ 3. Communication/i2c/i2c_atmega_328p_slave/i2c_atmega_328p_slave/i2c_atmega_328p_slave.h
@@ -12,13 +12,15 @@
 #include<avr/interrupt.h>
 #include <stdint.h>
 
-
+volatile uint8_t data;
+volatile uint8_t data2;
 
 // Function prototypes
 
 void i2c_init(uint8_t address);					// Enable I2C.
 void i2c_disable(void);							// Disable I2C.	
 void i2c_transmit_data(int data);			// Write data into buffer.
-void I2C_setCallbacks(void (*reciece_call)(uint8_t), void (*request_call)());  // setting uo callbacks
+
+
 
 #endif /* I2C_ATMEGA_328P_SLAVE_H_ */
