0fce68de4365a811d0ab6e1c6de2f007e98adf34
Jacobllc <57322518+Jacobllc@users.noreply.github.com>
2019-11-11 20:11:50 +0100

    Revert "slave"
    This reverts commit d82a5da4cb46ac40385cf66e753d50690a05fc16.

--- 3. Communication/i2c/i2c_atmega_328p_master/i2c_atmega_328p_master/main.c
+++ 3. Communication/i2c/i2c_atmega_328p_master/i2c_atmega_328p_master/main.c
@@ -23,20 +23,24 @@
 	
 	 while (1)
 	 {
-		printf("input a number\n");
+	 
+		
+		
+		printf("input an number\n");
 		scanf("%d",&data);
 		
-		i2c_start(0x40<<1 | I2C_WRITE);			//starts i2c comm here in writing mode
-		i2c_write(data);						//send pressed key char
-		i2c_stop();								//stop i2c here
+	
+		if(data>=256)
+		{
+			
+		byte=2;
+		uint8_t data_8bit[0];
 		
-		i2c_start(0x40<<1 | I2C_READ);			//here we start i2c in read mode where we request a byte
-		data_received=i2c_read_ack();			//read the byte
-		printf("Received: %d\n", data_received);
-		i2c_read_nack();						// read the neg ack
+		data_8bit[0]=(uint8_t)data;		// 8 LSB	
+		data_8bit[1]=data >> 8;			// 8 MSB
+		printf("Data first 8 bits %d is, data last 8 bits %d \n",data_8bit[0] , data_8bit[1]);
 		
-		i2c_stop();
-		
+		}
 	 }
 	 
  }
@@ -45,5 +49,15 @@
 
 
 /*
-
+		 printf("input a number\n");
+		 scanf("%d",&data);
+		 printf("Thanks\n");
+		 i2c_start(0x10<<1 | I2C_WRITE);		//starts i2c comm here in writing mode
+		 i2c_write(data);						//send pressed key char
+		 i2c_stop();							//stop i2c here
+			
+		 i2c_start(0x10<<1 | I2C_READ);			//here we start i2c in read mode where we request a byte
+		 data_received=i2c_read_ack();			//read the byte
+		 i2c_read_nack();						// read the neg ack
+		 i2c_stop();
 		*/
